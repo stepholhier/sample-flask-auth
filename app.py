b4f8384f625f4a1777d312f42a39e45d1db2acdd
Stephano <stegoodds@gmail.com>
2024-03-06 19:42:25 -0300

    Updating app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
@@ -11,6 +11,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 login_manager = LoginManager()
 db.init_app(app)
 login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
 @app.route('/login', methods=["POST"])
 def login():
@@ -23,9 +28,32 @@ def login():
         user = User.query.filter_by(username=username).first()
 
         if user.password == password:
-                return jsonify({"message": "Autenticação realizada com sucesso"})
+            login_user(user)
+            print(current_user.is_authenticated)
+            return jsonify({"message": "Autenticação realizada com sucesso"})
     
     return jsonify({"message": "Credenciais inválidas"}), 400
+
+@app.route('/logout', methods=["GET"])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout realizado com sucesso"})
+
+@app.route('/user', methods=["POST"])
+@login_required
+def create_user():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({"message": "Usuário cadastrado com sucesso"})
+
+    return jsonify({"message": "Dados inválidos"}), 400
 
 @app.route("/hello-world", methods=["GET"])
 def hello_world():
